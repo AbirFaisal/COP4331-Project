b65e4b6989252f251f2bc6a94cdbb2beb7cd0ae3
bryan-barreto <bryan94barreto@gmail.com>
2023-10-21 00:48:52 -0400

    Render loads.

--- TTT3D/src/main/java/edu/fau/eng/cop4331/ttt3d/app/views/GameView.java
+++ TTT3D/src/main/java/edu/fau/eng/cop4331/ttt3d/app/views/GameView.java
@@ -2,16 +2,88 @@ package edu.fau.eng.cop4331.ttt3d.app.views;
 
 
 import edu.fau.eng.cop4331.ttt3d.app.View;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.glfw.GLFWVidMode;
+import org.lwjgl.opengl.GL;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.system.MemoryStack;
 
 import javax.swing.*;
 import java.awt.*;
+import java.nio.IntBuffer;
 
-import static org.lwjgl.opengl.GL11.glColor4f;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryStack.stackPush;
+import static org.lwjgl.system.MemoryUtil.NULL;
 
 public class GameView implements View {
+
     @Override
     public void updateView() {
-//        glColor4f(1,1,1,1);
+        glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
+
+        glfwSwapBuffers(window); // swap the color buffers
+
+        // Poll for window events. The key callback above will only be
+        // invoked during this call.
+        glfwPollEvents();
+    }
+
+    private long window;
+    public void init(){
+        // Setup an error callback. The default implementation
+        // will print the error message in System.err.
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        // Initialize GLFW. Most GLFW functions will not work before doing this.
+        if ( !glfwInit() )
+            throw new IllegalStateException("Unable to initialize GLFW");
+
+        // Configure GLFW
+        glfwDefaultWindowHints(); // optional, the current window hints are already the default
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // the window will stay hidden after creation
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
+
+        // Create the window
+        window = glfwCreateWindow(300, 300, "Hello World!", NULL, NULL);
+        if ( window == NULL )
+            throw new RuntimeException("Failed to create the GLFW window");
+
+        glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
+            if ( key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE )
+                glfwSetWindowShouldClose(window, true); // We will detect this in the rendering loop
+        });
+
+        // Get the thread stack and push a new frame
+        try ( MemoryStack stack = stackPush() ) {
+            IntBuffer pWidth = stack.mallocInt(1); // int*
+            IntBuffer pHeight = stack.mallocInt(1); // int*
+
+            // Get the window size passed to glfwCreateWindow
+            glfwGetWindowSize(window, pWidth, pHeight);
+
+            // Get the resolution of the primary monitor
+            GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+            // Center the window
+            glfwSetWindowPos(
+                    window,
+                    (vidmode.width() - pWidth.get(0)) / 2,
+                    (vidmode.height() - pHeight.get(0)) / 2
+            );
+        } // the stack frame is popped automatically
+
+        // Make the OpenGL context current
+        glfwMakeContextCurrent(window);
+        // Enable v-sync
+        glfwSwapInterval(1);
+
+        // Make the window visible
+        glfwShowWindow(window);
+
+        GL.createCapabilities();
     }
 
     public GameView(JFrame window){
@@ -33,8 +105,9 @@ public class GameView implements View {
         window.add(renderPanel, BorderLayout.CENTER);
         window.setSize(600, 600);
         window.setLocationRelativeTo(null);
-        window.setVisible(true);
+        window.setVisible(false);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        init();
     }
     // class that defines a view of the game
 

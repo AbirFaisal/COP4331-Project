98eec695f51bcb38b327e4e761af83366adc3bac
Abir Faisal <AbirFaisal@users.noreply.github.com>
2023-12-02 07:13:20 -0500

    Revert "Server Implementation"
    This reverts commit bf5efc1750fc3d69dd2dea4d8bfe807ac29cc8a5.

--- TTT3D/src/main/java/edu/fau/eng/cop4331/ttt3d/server/Server.java
+++ TTT3D/src/main/java/edu/fau/eng/cop4331/ttt3d/server/Server.java
@@ -1,51 +1,14 @@
 package edu.fau.eng.cop4331.ttt3d.server;
 
-import java.net.*;
-import java.io.*;
-
 public class Server {
-    private ServerSocket server = null;
-    private Socket socket = null;
-    private DataInputStream in = null;
-    private DataOutputStream out = null;
 
-    public Server(int port) {
-        try {
-            server = new ServerSocket(port);
-            System.out.println("Server started");
 
-            System.out.println("Waiting for a client ...");
+    //Client clients[]
 
-            socket = server.accept();
-            System.out.println("Client accepted");
+    //recieve something
+    //how to handle it
 
-            in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
-            out = new DataOutputStream(socket.getOutputStream());
+    public Server() {
 
-            String line = "";
-
-            while (!line.equals("Over")) {
-                try {
-                    line = in.readUTF();
-                    System.out.println("Received from client: " + line);
-
-                    // send the message back to the client
-                    out.writeUTF("Server: " + line);
-                } catch (IOException i) {
-                    System.out.println(i);
-                }
-            }
-            System.out.println("Closing connection");
-
-            socket.close();
-            in.close();
-            out.close();
-        } catch (IOException i) {
-            System.out.println(i);
-        }
-    }
-
-    public static void main(String args[]) {
-        new Server(5000);
     }
 }

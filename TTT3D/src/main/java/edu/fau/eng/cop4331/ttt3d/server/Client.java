bf5efc1750fc3d69dd2dea4d8bfe807ac29cc8a5
JamiliusCaesar <jfarrington2023@fau.edu>
2023-12-02 01:21:50 -0500

    Server Implementation
    A server file and a client file.

---
+++ TTT3D/src/main/java/edu/fau/eng/cop4331/ttt3d/server/Client.java
@@ -0,0 +1,54 @@
+package edu.fau.eng.cop4331.ttt3d.server;
+
+import java.io.*;
+import java.net.*;
+
+public class Client {
+    private Socket socket = null;
+    private DataInputStream input = null;
+    private DataOutputStream out = null;
+
+    public Client(String address, int port) {
+        try {
+            socket = new Socket(address, port);
+            System.out.println("Connected");
+
+            input = new DataInputStream(System.in);
+            out = new DataOutputStream(socket.getOutputStream());
+        } catch (UnknownHostException u) {
+            System.out.println(u);
+            return;
+        } catch (IOException i) {
+            System.out.println(i);
+            return;
+        }
+
+        String line = "";
+
+        while (!line.equals("Over")) {
+            try {
+                System.out.print("Enter a message: ");
+                line = input.readLine();
+                out.writeUTF(line);
+
+                // Receive and print the message from the server
+                String serverResponse = input.readUTF();
+                System.out.println("Server response: " + serverResponse);
+            } catch (IOException i) {
+                System.out.println(i);
+            }
+        }
+
+        try {
+            input.close();
+            out.close();
+            socket.close();
+        } catch (IOException i) {
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String args[]) {
+        new Client("127.0.0.1", 5000);
+    }
+}
--- TTT3D/src/main/java/edu/fau/eng/cop4331/ttt3d/server/Server.java
+++ TTT3D/src/main/java/edu/fau/eng/cop4331/ttt3d/server/Server.java
@@ -1,14 +1,51 @@
 package edu.fau.eng.cop4331.ttt3d.server;
 
+import java.net.*;
+import java.io.*;
+
 public class Server {
+    private ServerSocket server = null;
+    private Socket socket = null;
+    private DataInputStream in = null;
+    private DataOutputStream out = null;
 
+    public Server(int port) {
+        try {
+            server = new ServerSocket(port);
+            System.out.println("Server started");
 
-    //Client clients[]
+            System.out.println("Waiting for a client ...");
 
-    //recieve something
-    //how to handle it
+            socket = server.accept();
+            System.out.println("Client accepted");
 
-    public Server() {
+            in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+            out = new DataOutputStream(socket.getOutputStream());
 
+            String line = "";
+
+            while (!line.equals("Over")) {
+                try {
+                    line = in.readUTF();
+                    System.out.println("Received from client: " + line);
+
+                    // send the message back to the client
+                    out.writeUTF("Server: " + line);
+                } catch (IOException i) {
+                    System.out.println(i);
+                }
+            }
+            System.out.println("Closing connection");
+
+            socket.close();
+            in.close();
+            out.close();
+        } catch (IOException i) {
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String args[]) {
+        new Server(5000);
     }
 }
